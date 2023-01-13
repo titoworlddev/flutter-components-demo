6b7113068fd82b437ba1aee5d348fdd0bddb9d53
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:58:47 +0100

    Fixed routes

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -10,16 +10,15 @@ import 'package:componentesab/src/pages/slider_page.dart';
 import 'package:componentesab/src/pages/listview_page.dart';
 
 Map<String, WidgetBuilder> getAplicationRoutes() {
-
- return <String, WidgetBuilder>{
-    '/'      : (BuildContext context) => HomePage(),
-    'alert'  : (BuildContext context) => AlertPage(),
-    'avatar' : (BuildContext context) => AvatarPage(),
-    'card'   : (BuildContext context) => CardPage(),
-    'animatedContainer'   : (BuildContext context) => AnimatedContainerPage(),
-    'inputs'   : (BuildContext context) => InputPage(),
-    'slider'   : (BuildContext context) => SliderPage(),
-    'list'   : (BuildContext context) => ListaPage(),
-};
-
+  return <String, WidgetBuilder>{
+    '/': (BuildContext context) => const HomePage(),
+    'alert': (BuildContext context) => const AlertPage(),
+    'avatar': (BuildContext context) => const AvatarPage(),
+    'card': (BuildContext context) => const CardPage(),
+    'animatedContainer': (BuildContext context) =>
+        const AnimatedContainerPage(),
+    'inputs': (BuildContext context) => const InputPage(),
+    'slider': (BuildContext context) => const SliderPage(),
+    'list': (BuildContext context) => const ListaPage(),
+  };
 }
