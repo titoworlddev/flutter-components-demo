135c613c9531567f3ac9b98cd5da0f8ef83b7d68
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:53:05 +0100

    Fixed listview page

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -3,15 +3,16 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 
 class ListaPage extends StatefulWidget {
+  const ListaPage({super.key});
+
   @override
-  _ListaPageState createState() => _ListaPageState();
+  State<ListaPage> createState() => _ListaPageState();
 }
 
 class _ListaPageState extends State<ListaPage> {
+  ScrollController scrollController = ScrollController();
 
-  ScrollController _scrollController = new ScrollController();
-
-  List<int> _listaNumeros = new List();
+  List<int> listaNumeros = [];
   int _ultimoItem = 0;
   bool _isLoading = false;
 
@@ -21,142 +22,103 @@ class _ListaPageState extends State<ListaPage> {
 
     _agregar10();
 
-    _scrollController.addListener(() {
-
-      if( _scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+    scrollController.addListener(() {
+      if (scrollController.position.pixels ==
+          scrollController.position.maxScrollExtent) {
         // _agregar10();
         fetchData();
       }
-
     });
   }
 
   @override
   void dispose() {
     super.dispose();
-    _scrollController.dispose();
+    scrollController.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Listas'),
-      ),
-      body: Stack(
-        children: <Widget>[
-          _crearLista(),
-          _crearLoading()
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: const Text('Listas'),
+        ),
+        body: Stack(
+          children: <Widget>[_crearLista(), _crearLoading()],
+        ));
   }
 
   Widget _crearLista() {
-
-
     return RefreshIndicator(
-
-        onRefresh: obtenerPagina1,
-
-        child: ListView.builder(
-        controller: _scrollController,
-        itemCount: _listaNumeros.length,
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+        controller: scrollController,
+        itemCount: listaNumeros.length,
         itemBuilder: (BuildContext context, int index) {
-
-          final imagen = _listaNumeros[index];
+          final imagen = listaNumeros[index];
 
           return FadeInImage(
-            image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
-            placeholder: AssetImage('assets/original.gif') 
-          );
+              image:
+                  NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
+              placeholder: const AssetImage('assets/original.gif'));
         },
       ),
     );
-
   }
 
-  Future<Null> obtenerPagina1() async {
+  Future obtenerPagina1() async {
+    const duration = Duration(seconds: 2);
 
-    final duration = new Duration( seconds: 2);
-
-    new Timer( duration, (){
-
-      _listaNumeros.clear();
+    Timer(duration, () {
+      listaNumeros.clear();
       _ultimoItem++;
       _agregar10();
-
     });
 
     return Future.delayed(duration);
-
   }
 
   void _agregar10() {
-
     for (var i = 1; i < 10; i++) {
       _ultimoItem++;
-      _listaNumeros.add(_ultimoItem);
+      listaNumeros.add(_ultimoItem);
     }
 
     setState(() {});
-
   }
 
-
-
-  Future<Null> fetchData() async {
-
-    _isLoading =true;
+  Future fetchData() async {
+    _isLoading = true;
     setState(() {});
 
-    final duration = new Duration( seconds: 2 );
-    new Timer(duration, respuestaHTTP );
-
+    const duration = Duration(seconds: 2);
+    Timer(duration, respuestaHTTP);
   }
 
   void respuestaHTTP() {
-
     _isLoading = false;
 
-    _scrollController.animateTo(
-      _scrollController.position.pixels + 100,
-      curve: Curves.fastOutSlowIn,
-      duration: Duration( milliseconds: 250 )
-    );
-
+    scrollController.animateTo(scrollController.position.pixels + 100,
+        curve: Curves.fastOutSlowIn,
+        duration: const Duration(milliseconds: 250));
 
     _agregar10();
-
   }
 
-
   Widget _crearLoading() {
-
-    if ( _isLoading ) {
-
+    if (_isLoading) {
       return Column(
         mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[CircularProgressIndicator()
-            ]
-          ),
-          SizedBox(
-            height: 15.0
-          )
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [CircularProgressIndicator()]),
+          const SizedBox(height: 15.0)
         ],
-      ); 
-      
-      
-
+      );
     } else {
-
       return Container();
     }
-
   }
-
 }
