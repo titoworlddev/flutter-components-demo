ea912ef0ddb662cbc950aaa3ce5f18563e87fa1e
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:23:58 +0100

    Fixed alert page

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -1,67 +1,61 @@
 import 'package:flutter/material.dart';
 
 class AlertPage extends StatelessWidget {
-  
-  
+  const AlertPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text( 'Alert Page' ),
+        title: const Text('Alert Page'),
       ),
       body: Center(
-        child: RaisedButton(
-          child: Text('Mostrar alerta'),
-          color: Colors.blue,
-          textColor: Colors.white,
-          shape: StadiumBorder(),
-          onPressed: () => _mostrarAlert(context),
-        )
-      ),
+          child: TextButton(
+        style: TextButton.styleFrom(
+            backgroundColor: Colors.blue,
+            shape: const StadiumBorder(),
+            textStyle: const TextStyle(color: Colors.white)),
+        onPressed: () => _mostrarAlert(context),
+        child: const Text('Mostrar alerta'),
+      )),
       floatingActionButton: FloatingActionButton(
-        child: Icon( Icons.keyboard_arrow_left ),
-        onPressed: (){
+        child: const Icon(Icons.keyboard_arrow_left),
+        onPressed: () {
           Navigator.pop(context);
         },
       ),
     );
-
   }
-
-
 
   void _mostrarAlert(BuildContext context) {
-
     showDialog(
-      context: context,
-      barrierDismissible: true,
-      builder: (context) { 
-
-        return AlertDialog(
-          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0) ),
-          title: Text('Titulo'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text('Este es el contenido de la caja de la alerta'),
-              FlutterLogo( size: 100.0)
-            ],
-          ),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Cancelar'),
-              onPressed: ()=> Navigator.of(context).pop(), 
+        context: context,
+        barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            title: const Text('Titulo'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: const [
+                Text('Este es el contenido de la caja de la alerta'),
+                FlutterLogo(size: 100.0)
+              ],
+            ),
+            actions: [
+              TextButton(
+                child: const Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop(),
               ),
-            FlatButton(
-              child: Text('Ok'),
-              onPressed: (){ 
-                Navigator.of(context).pop();
-              }, 
-            )
-          ],
-        );
-      }
-    );
+              TextButton(
+                child: const Text('Ok'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
-
 }
