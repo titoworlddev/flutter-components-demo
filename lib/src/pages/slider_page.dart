46479736be1c6515c50e5202104c910a7ee435af
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:54:06 +0100

    Fixed slider page

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -1,41 +1,32 @@
 import 'package:flutter/material.dart';
 
-
-
 class SliderPage extends StatefulWidget {
+  const SliderPage({super.key});
+
   @override
-  _SliderPageState createState() => _SliderPageState();
+  State<SliderPage> createState() => _SliderPageState();
 }
 
 class _SliderPageState extends State<SliderPage> {
-
   double _valorSlider = 200.0;
   bool _bloquearCheck = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sliders')
-      ),
+      appBar: AppBar(title: const Text('Sliders')),
       body: Container(
-        padding: EdgeInsets.only(top:50.0),
-        child: Column(
-          children: <Widget>[
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Column(children: <Widget>[
             _crearSlider(),
             _crearCheckBox(),
             _crearSwitch(),
-            Expanded(
-              child: _crearImagen()
-              ),
-          ]
-        )
-      ),
+            Expanded(child: _crearImagen()),
+          ])),
     );
   }
 
   Widget _crearSlider() {
-
     return Slider(
       activeColor: Colors.indigoAccent,
       label: 'Tamaño de la imagen',
@@ -43,56 +34,43 @@ class _SliderPageState extends State<SliderPage> {
       value: _valorSlider,
       min: 10.0,
       max: 400.0,
-      onChanged: (_bloquearCheck) ? null : ( valor ){
-        setState(() {
-          _valorSlider = valor;
-        });
-      },
+      onChanged: (_bloquearCheck)
+          ? null
+          : (valor) {
+              setState(() {
+                _valorSlider = valor;
+              });
+            },
     );
-
   }
 
   Widget _crearCheckBox() {
-
-    // return Checkbox(
-    //   value: _bloquearCheck, 
-    //   onChanged: (valor) {
-    //     setState(() {
-    //     _bloquearCheck = valor;
-    //     });
-    //   }
-    // );
     return CheckboxListTile(
-      title: Text('Bloquear Slider'),
-      value: _bloquearCheck, 
-      onChanged: (valor) {
-        setState(() {
-        _bloquearCheck = valor;
+        title: const Text('Bloquear Slider'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
+          setState(() {
+            _bloquearCheck = valor!;
+          });
         });
-      }
-    );
   }
 
   Widget _crearSwitch() {
-
     return SwitchListTile(
-      title: Text('Bloquear Slider'),
-      value: _bloquearCheck, 
-      onChanged: (valor) {
-        setState(() {
-        _bloquearCheck = valor;
+        title: const Text('Bloquear Slider'),
+        value: _bloquearCheck,
+        onChanged: (valor) {
+          setState(() {
+            _bloquearCheck = valor;
+          });
         });
-      }
-    );
-
   }
 
   Widget _crearImagen() {
-
     return Image(
-      image: NetworkImage('https://i.dlpng.com/static/png/1339647-batman-png-batman-png-1404_1500_preview.png'),
+      image: const NetworkImage(
+          'https://i.dlpng.com/static/png/1339647-batman-png-batman-png-1404_1500_preview.png'),
       width: _valorSlider,
     );
   }
-
 }
