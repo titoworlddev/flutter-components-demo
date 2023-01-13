c37e52f9831b84c52e9b7c34ace17434623320cc
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:58:16 +0100

    Fixed home temp

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,59 +1,34 @@
 import 'package:flutter/material.dart';
 
-
-
 class HomePageTemp extends StatelessWidget {
+  const HomePageTemp({super.key});
 
-  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Componentes Temp'), 
+        title: const Text('Componentes Temp'),
       ),
-      body: ListView(
-        // children: _crearitems()
-        children: _crearitemscorto()
-      ),
+      body: ListView(children: _crearitemscorto()),
     );
   }
 
-  // List<Widget> _crearitems() {
-
-  //   List<Widget> lista = new List<Widget>();
-
-  //   for (String opt in opciones) {
-      
-  //     final tempWidget = ListTile(
-  //       title: Text( opt ),
-  //     );
-  //     lista..add( tempWidget )
-  //          ..add( Divider() );
-  //   }
-
-  //   return lista;
-  // }
-
   List<Widget> _crearitemscorto() {
+    final List<String> opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
 
-    return opciones.map( ( item ){
-      
+    return opciones.map((item) {
       return Column(
         children: <Widget>[
           ListTile(
-            title: Text( item + '!' ),
-            subtitle: Text('Cualquier cosa'),
-            leading: Icon( Icons.account_balance_wallet),
-            trailing: Icon(Icons.keyboard_arrow_right),
-            onTap: (){ },
+            title: Text('$item!'),
+            subtitle: const Text('Cualquier cosa'),
+            leading: const Icon(Icons.account_balance_wallet),
+            trailing: const Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
           ),
-          Divider()
+          const Divider()
         ],
       );
     }).toList();
-    
   }
-
-
 }
