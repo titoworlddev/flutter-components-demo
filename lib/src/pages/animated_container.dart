0ba9e7b3ff8aa24884f1d87646591e762f9e35e8
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:57:53 +0100

    Fixed animated container

--- lib/src/pages/animated_container.dart
+++ lib/src/pages/animated_container.dart
@@ -2,64 +2,52 @@ import 'package:flutter/material.dart';
 
 import 'dart:math';
 
-
-
 class AnimatedContainerPage extends StatefulWidget {
+  const AnimatedContainerPage({super.key});
+
   @override
-  _AnimatedContainerPageState createState() => _AnimatedContainerPageState();
+  State<AnimatedContainerPage> createState() => _AnimatedContainerPageState();
 }
 
 class _AnimatedContainerPageState extends State<AnimatedContainerPage> {
   double _width = 50.0;
   double _height = 50.0;
-  Color _color= Colors.pink;
+  Color _color = Colors.pink;
 
   BorderRadiusGeometry _borderRadius = BorderRadius.circular(8.0);
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Animated Container'),
+        title: const Text('Animated Container'),
       ),
       body: Center(
         child: AnimatedContainer(
-          duration: Duration(seconds: 1),
+          duration: const Duration(seconds: 1),
           curve: Curves.fastOutSlowIn,
           width: _width,
           height: _height,
-          decoration: BoxDecoration(
-            borderRadius: _borderRadius,
-            color: _color
-          ),
+          decoration: BoxDecoration(borderRadius: _borderRadius, color: _color),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.play_arrow),
         onPressed: _cambiarForma,
-        ),
+        child: const Icon(Icons.play_arrow),
+      ),
     );
   }
-  
-  void _cambiarForma() {
 
+  void _cambiarForma() {
     final random = Random();
 
     setState(() {
-      
       _width = random.nextInt(300).toDouble();
       _height = random.nextInt(300).toDouble();
       _color = Color.fromRGBO(
-        random.nextInt(255),
-        random.nextInt(255),
-        random.nextInt(255),
-        1
-        );
+          random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
 
-        _borderRadius = BorderRadius.circular( random.nextInt(100).toDouble());
-
+      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
     });
-
   }
 }
