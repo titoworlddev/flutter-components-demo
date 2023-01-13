364ce7cd6a77fca5778f608735a803368531ea14
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:58:31 +0100

    Fixed menu provider

--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -2,25 +2,21 @@ import 'package:flutter/services.dart' show rootBundle;
 
 import 'dart:convert';
 
-
-
 class _MenuProvider {
   _MenuProvider() {
-
-    // cargardata();
+    cargardata();
   }
 
   List<dynamic> opciones = [];
 
   Future<List<dynamic>> cargardata() async {
-    
     final resp = await rootBundle.loadString('data/menu_opts.json');
-      
-      Map dataMap = json.decode( resp );
-      opciones = dataMap['rutas'];
+
+    Map dataMap = json.decode(resp);
+    opciones = dataMap['rutas'];
 
     return opciones;
   }
 }
 
-final menuProvider = new _MenuProvider();
+final menuProvider = _MenuProvider();
