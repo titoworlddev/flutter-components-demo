9ae4376371c1514cfd964bdedb7d3ed906673250
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:28:31 +0100

    Fixed homepage

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,64 +4,49 @@ import 'package:componentesab/src/providers/menu_provider.dart';
 
 import 'package:componentesab/src/utils/icono_string_util.dart';
 
-// import 'package:componentes/src/pages/alert_page.dart';
-
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar : AppBar(
-        title: Text('Componentes'),
+      appBar: AppBar(
+        title: const Text('Componentes'),
       ),
       body: _lista(),
     );
   }
 
   Widget _lista() {
-
-    // menuProvider.cargarData ()
     return FutureBuilder(
       future: menuProvider.cargardata(),
-      initialData: [],
-      builder: ( context, AsyncSnapshot<List<dynamic>> snapshot ){
-      
-      return ListView(
-      children: _listaitems( snapshot.data, context ),
+      initialData: const [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return ListView(
+          children: _listaitems(snapshot.data, context),
         );
       },
     );
-
-    
   }
 
-  List<Widget> _listaitems( List<dynamic> data, BuildContext context ) {
-    
+  List<Widget> _listaitems(List<dynamic>? data, BuildContext context) {
     final List<Widget> opciones = [];
-    
-    data.forEach( (opt) {
+
+    for (var opt in data!) {
       final widgetTemp = ListTile(
-        title   : Text( opt['texto']),
-        leading : getIcon( opt['icon']),
-        trailing: Icon ( Icons.keyboard_arrow_right, color: Colors.blue ),
+        title: Text(opt['texto']),
+        leading: getIcon(opt['icon']),
+        trailing: const Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: () {
-
-          Navigator.pushNamed(context, opt['ruta'] );
-          // final route = MaterialPageRoute(
-          //   builder: ( context )=> AlertPage()
-            
-          // );
-          // Navigator.push(context, route);
-
+          Navigator.pushNamed(context, opt['ruta']);
         },
       );
 
-      opciones..add( widgetTemp )
-              ..add( Divider () );
-    });
+      opciones
+        ..add(widgetTemp)
+        ..add(const Divider());
+    }
 
     return opciones;
   }
-
-  
 }
