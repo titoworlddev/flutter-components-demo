6492b9e159d75856b6a9b7b80305b63a79eeb668
titoworlddev <titocristian.dev@gmail.com>
2023-01-13 10:58:04 +0100

    Fixed avatar page

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,41 +1,39 @@
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
-  
-  
+  const AvatarPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
-        title: Text( 'Avatar Page' ),
+        title: const Text('Avatar Page'),
         actions: <Widget>[
-
           Container(
-            padding: EdgeInsets.all(8.0),
-            child: CircleAvatar(
-              backgroundImage: NetworkImage('https://kabina34radio.com/wp-content/uploads/2019/05/URnaMrya.jpg'),
+            padding: const EdgeInsets.all(8.0),
+            child: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://kabina34radio.com/wp-content/uploads/2019/05/URnaMrya.jpg'),
               radius: 20.0,
             ),
           ),
           Container(
-            margin: EdgeInsets.only(right: 10.0),
-            child: CircleAvatar(
-              child: Text('SL'),
+            margin: const EdgeInsets.only(right: 10.0),
+            child: const CircleAvatar(
               backgroundColor: Colors.brown,
+              child: Text('SL'),
             ),
           )
         ],
       ),
-      body: Center(
+      body: const Center(
         child: FadeInImage(
-          image: NetworkImage('https://www.radiocantilo.com/wp-content/uploads/2018/11/STANNN.jpg'),
+          image: NetworkImage(
+              'https://www.radiocantilo.com/wp-content/uploads/2018/11/STANNN.jpg'),
           placeholder: AssetImage('assets/original.gif'),
-          fadeInDuration: Duration( milliseconds: 200 ),
-          
+          fadeInDuration: Duration(milliseconds: 200),
         ),
       ),
     );
-
   }
 }
